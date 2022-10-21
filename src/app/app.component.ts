8e0ca816531479f19c04cdd6567f1fd66f2c991a
Ashik Arefin <abdullahilashik@gmail.com>
2022-10-21 18:52:42 +0600

    sync

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'clipnshare_ng';
+// }
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>hello world</h1>`
 })
 export class AppComponent {
-  title = 'clipnshare_ng';
+
 }
