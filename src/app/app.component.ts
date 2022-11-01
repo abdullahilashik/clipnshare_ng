a09444b3dba8667fa2bd4d83cb6390394ad5a6ef
Ashik Arefin <abdullahilashik@gmail.com>
2022-11-01 20:52:53 +0600

    var

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',  
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  title = 'Angular JS';
+  title = 'Angular 14';
+  imageUrl = 'https://picsum.photos/id/1/200/200';
+  name = 'n/a';
+
+  getName(){
+    return this.title;
+  }
+
+  getPhotos(){
+    var imageUrls = [];
+
+    for(let i=0;i<50;i++){
+      imageUrls.push('https://picsum.photos/id/'+i+1+'/300/300/')
+    }
+    return imageUrls;
+  }
+
+  changeName(e: KeyboardEvent){
+    console.log(e);
+    // this.name = e.target.value;
+  }
 }
